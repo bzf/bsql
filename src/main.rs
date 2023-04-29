95971eef5d70346e994cbc787d97d3b1c1e0b95e
André Ligné <andre@ligne.io>
2023-04-29 11:06:49 +0200

    Add REPL to main()

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
+use std::io::Write;
+
 fn main() {
-    println!("Hello, world!");
+    loop {
+        let expression = prompt("> ");
+
+        match &expression[..] {
+            "" => continue,
+            "exit" => break,
+
+            _ => println!("Read statement: '{}'", expression),
+        }
+    }
+}
+
+fn prompt(name: &str) -> String {
+    let mut line = String::new();
+    print!("{}", name);
+
+    std::io::stdout().flush().unwrap();
+    std::io::stdin()
+        .read_line(&mut line)
+        .expect("Error: Could not read a line");
+
+    return line.trim().to_string();
 }
