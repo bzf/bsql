a516517a166571d4d73edc83c2dbe5ec40ddf89b
André Ligné <andre@ligne.io>
2023-05-04 15:00:43 +0200

    Add metadata array to InternalPage

--- src/internal/page.rs
+++ src/internal/page.rs
@@ -1,11 +1,15 @@
 pub type PageData = [u8; 4096];
 
 pub struct InternalPage {
+    pub metadadata: PageData,
     pub data: PageData,
 }
 
 impl InternalPage {
     pub fn new() -> Self {
-        Self { data: [0; 4096] }
+        Self {
+            metadadata: [0; 4096],
+            data: [0; 4096],
+        }
     }
 }
