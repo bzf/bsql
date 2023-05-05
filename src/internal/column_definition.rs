37235a5dca0a5dd2a786893e458ba0016ab1cb8c
André Ligné <andre@ligne.io>
2023-05-05 07:31:30 +0200

    Add methods for serializing ColumnDefinition to bytes

--- src/internal/column_definition.rs
+++ src/internal/column_definition.rs
@@ -14,11 +14,46 @@ impl ColumnDefinition {
         }
     }
 
+    pub fn from_raw_bytes(bytes: &[u8]) -> Option<ColumnDefinition> {
+        let [column_id, data_type_id] = bytes else {
+            return None;
+        };
+
+        Some(Self {
+            column_id: *column_id,
+            data_type: DataType::from_type_id(*data_type_id)?,
+        })
+    }
+
+    pub fn to_raw_bytes(&self) -> Vec<u8> {
+        let mut column_definition = Vec::with_capacity(2);
+        column_definition.push(self.column_id);
+        column_definition.push(self.data_type().bsql_type_id());
+
+        return column_definition;
+    }
+
     pub fn column_id(&self) -> u8 {
         self.column_id
     }
 
     pub fn data_type(&self) -> &DataType {
         &self.data_type
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_serializing_and_deserializing_into_bytes() {
+        let column_definition = ColumnDefinition::new(1, DataType::Integer);
+
+        let serialized_column: Vec<u8> = column_definition.to_raw_bytes();
+        assert_eq!(vec![0x1, 0x1], serialized_column);
+
+        let deserialized_column = ColumnDefinition::from_raw_bytes(&serialized_column);
+        assert_eq!(Some(column_definition), deserialized_column);
     }
 }
--- src/internal/data_type.rs
+++ src/internal/data_type.rs
@@ -14,6 +14,14 @@ impl std::fmt::Display for DataType {
 }
 
 impl DataType {
+    pub fn from_type_id(type_id: u8) -> Option<Self> {
+        match type_id {
+            1 => Some(DataType::Integer),
+
+            _ => None,
+        }
+    }
+
     pub fn to_bsql_value(data_type: &Self, data: &[u8]) -> Option<Value> {
         match data_type {
             DataType::Integer => Some(Value::Integer(*data.first()?)),
