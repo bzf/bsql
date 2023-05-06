af2216ea9ddbbce6274acd13ce2ca72a22173bc4
André Ligné <andre@ligne.io>
2023-05-06 16:18:29 +0200

    Add simple integration test

---
+++ tests/integration_test.rs
@@ -0,0 +1,35 @@
+use bsql::{Manager, QueryResult};
+
+#[test]
+fn test_creating_database_table_and_inserting_rows() {
+    let mut manager = Manager::new();
+
+    let database_name = "drinkr";
+    manager
+        .execute("", &format!("CREATE DATABASE {};", database_name))
+        .expect("Failed to create database");
+
+    let table_name = "brands";
+    manager
+        .execute(
+            database_name,
+            &format!("CREATE TABLE {} (brand_id integer);", table_name),
+        )
+        .expect("Failed to create table");
+
+    let number_of_brands = 32;
+    for brand_id in 0..number_of_brands {
+        manager
+            .execute(
+                database_name,
+                &format!("INSERT INTO {} VALUES ({});", table_name, brand_id),
+            )
+            .expect("Failed to insert rows into the table");
+    }
+
+    let Ok(QueryResult::RowResult(row_result)) = manager.execute(database_name, &format!("SELECT * FROM {};", table_name)) else {
+        return assert!(false, "Did not get the expected result");
+    };
+
+    assert_eq!(row_result.rows().len(), number_of_brands);
+}
